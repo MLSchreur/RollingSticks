26bcb14d1f0b6536a290646a77f1974b9254f2c1
Rebaf <Cor.Faber@gmail.com>
2017-03-14 22:23:57 +0100

    static index.html - for tekst implemented PUT and DELETE and changed save in service. Also corrected name package within src test java

--- src/main/java/nl/rollingsticks/persistence/TekstService.java
+++ src/main/java/nl/rollingsticks/persistence/TekstService.java
@@ -13,16 +13,25 @@ public class TekstService {
 	@Autowired
 	private TekstRepository tekstRepository;
 	
+//	public void save(Tekst tekst){
+//		tekstRepository.save(tekst);
+//	}
+//
+	public Tekst save(Tekst tekst){
+		return tekstRepository.save(tekst);
+	}
+
+	public Tekst findById(Long id) {
+		return tekstRepository.findOne(id);
+	}
+	
 	public Iterable <Tekst> findAll(){
 		Iterable <Tekst> result = tekstRepository.findAll();
 		return result;
 	}
-	
-	public void save(Tekst tekst){
-		tekstRepository.save(tekst);
-	}
 
-//	public Tekst save(Tekst tekst){
-//		return tekstRepository.save(tekst);
-//	}
+	public void deleteById(Long id) {
+		tekstRepository.delete(id);
+	}
+	
 }
--- src/main/java/nl/rollingsticks/rest/service/TekstEndpoint.java
+++ src/main/java/nl/rollingsticks/rest/service/TekstEndpoint.java
@@ -1,9 +1,12 @@
 package nl.rollingsticks.rest.service;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,9 +28,19 @@ public class TekstEndpoint {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response postTekst(Tekst tekst){
-		tekstService.save(tekst);
-		System.out.println("@POST: " + tekst.getTekst() + " - " + tekst.getId());
-		return Response.accepted(tekst).build();
+		System.out.println("pre@POST: (" + tekst.getId() + ") " + tekst.getTekst());
+		Tekst result = tekstService.save(tekst);
+		System.out.println("@POST: (" + result.getId() + ") " + result.getTekst());
+		return Response.accepted(result).build();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public Response getTekstById(@PathParam("id") Long id ) {
+		System.out.println("@GET: (" + id + ")");
+		Tekst result = this.tekstService.findById(id);
+		return Response.ok(result).build();
 	}
 	
 	@GET
@@ -36,5 +49,25 @@ public class TekstEndpoint {
 		System.out.println("@GET: Got the list!");
 		Iterable <Tekst> result = tekstService.findAll();
 		return Response.ok(result).build();
+	}
+	
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public Response deleteTekstById(@PathParam("id") Long id){
+		System.out.println("@DELETE: (" + id+ ")");
+		this.tekstService.deleteById(id);
+		return Response.accepted().build();
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response putTekst(Tekst tekst) {
+		System.out.println("pre@PUT: (" + tekst.getId() + ") " + tekst.getTekst());
+		this.tekstService.save(tekst);
+		Tekst result = tekstService.save(tekst);
+		System.out.println("@PUT: (" + result.getId() + ") " + result.getTekst());
+		return Response.accepted(result).build();
 	}
 }
