dc0ce677565811c770aaa7a8fe4c0b47242d7df2
Jase Terhaar <jase_terhaar@msn.com>
2017-03-30 10:16:16 +0200

    oplossen van verschillen

--- src/main/java/nl/rollingsticks/RollingsticksApplication.java
+++ src/main/java/nl/rollingsticks/RollingsticksApplication.java
@@ -2,12 +2,9 @@ package nl.rollingsticks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import nl.rollingsticks.domain.Storage;
 
 @SpringBootApplication
-@EnableConfigurationProperties(Storage.class)
 public class RollingsticksApplication {
 
 	public static void main(String[] args) {
